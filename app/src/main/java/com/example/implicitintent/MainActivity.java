42ba23b5aed49107ce1f53d54073fe8091a92803
riyas0607 <128897861+riyas0607@users.noreply.github.com>
2024-03-15 10:31:09 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.implicitintent;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.geeksforgeeks.org/"));
+        startActivity(intent);
     }
 }
